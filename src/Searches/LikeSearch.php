5ebbfeff45a7a11f3468b2b9be319b1814e6e63f
Muhaimenul Islam Ove <i.muhaimen@gmail.com>
2019-02-21 19:08:02 +0600

    resolved class namespace and some minor changes

--- src/Searches/LikeSearch.php
+++ src/Searches/LikeSearch.php
@@ -6,27 +6,23 @@
  * Date: 1/21/2019
  * Time: 11:15 PM
  */
-namespace Muhaimenul\Larasearch\Strategies;
+namespace Muhaimenul\Larasearch\Searches;
 
 class LikeSearch implements SearchStrategy
 {
     public function search($query, $term, $searchable)
     {
-        // TODO: Implement search() method.
+        $query->where($searchable[0], 'Like', '%' . $term . '%');
+        $length = count($searchable);
 
-        $query->where($searchable[0], 'Like', '%'.$term.'%');
-        $len = count($searchable);
-        if($len)
+        if($length > 1)
         {
-            for($i = 1; $i < $len; $i++)
+            for($i = 1; $i < $length; $i++)
             {
-                $query->orWhere($searchable[$i], 'Like', '%'.$term.'%');
+                $query->orWhere($searchable[$i], 'Like', '%' . $term . '%');
             }
         }
-//        $query->where(1,1);
-//        foreach ($searchable as $searchCol) {
-//            $query->orWhere($searchCol, 'Like', '%'.$term.'%');
-//        }
+
         return $query;
     }
 }
