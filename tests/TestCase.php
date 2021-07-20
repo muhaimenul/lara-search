63eaae1c289fca1e3bb84e663c73c9d23e2a0e32
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2021-07-20 19:17:49 +0600

    add test autoload dev

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,42 @@
 namespace Muhaimenul\Larasearch\Tests;
 
 //use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Muhaimenul\Larasearch\LarasearchServiceProvider;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 abstract class TestCase extends OrchestraTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        // Code before application created.
+
+        parent::setUp();
+
+        // Code after application created.
+    }
+
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            LarasearchServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // perform environment setup
+    }
 }
