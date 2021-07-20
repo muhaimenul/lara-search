2bbbde08b4830bf8730e0857a4337ad2a5970594
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2021-07-20 17:54:36 +0600

    added Orchestra test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,10 @@
 
 namespace Muhaimenul\Larasearch\Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+//use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends OrchestraTestCase
 {
     use CreatesApplication;
 }
